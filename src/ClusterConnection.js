8e3683fd8702fe35fe9a01fcce840d00055bde6f
Sergio Rodríguez Vázquez <sergio.rodriguezvazquez@alum.uca.es>
2024-01-30 18:23:39 +0100

    Basic operations from DB

--- src/ClusterConnection.js
+++ src/ClusterConnection.js
@@ -16,14 +16,64 @@ export async function connectToCluster(uri) {
     }
  }
 
+ // This function creates a database and collection if they do not exist.
  export async function executeUserCrudOperations() {
-    // const uri = process.env.DB_URI;
-    const uri = "mongodb+srv://sergiorodriguezvazquez:2F8tvbJd69oScQ9h@preventaidb.ffgcgti.mongodb.net/?retryWrites=true&w=majority";
+    const uri = process.env.DB_CODE_URI;
     let mongoClient;
+
+   try {
+       mongoClient = await connectToCluster(uri);
+       const db = mongoClient.db('user');
+       const collection = db.collection('tecnhicians');
+        console.log('Connected to user.tecnhicians collection');
+        const name = "joce antonio";
+    // CREATE AN USER
+    //    console.log('CREATE Technician');
+    //    await createUserTechnician(collection);
+
+    //Find a user by name in the database
+    console.log(await findTechnicianByName(collection, 'John Smith'));
+
+    console.log('UPDATE Student\'s Password');
+    await updateTecnhiciansByName(collection, 'John Smith', { password: '1234' });
+    console.log(await findTechnicianByName(collection, 'John Smith'));
+
+    console.log('DELETE Student');
+    await deleteTecnhiciansByName(collection, 'John Smith');
+    console.log(await findTechnicianByName(collection, 'John Smith'));
+    
+    return name;
+
+   } finally {
+       await mongoClient.close();
+   }
+}
+
+//Insert a single Technician into the collection
+export async function createUserTechnician(collection) {
+    const Tecnhician = {
+        name: 'John Smith',
+        id_user: 1,
+        password: '123456789',
+    };
  
-    try {
-        mongoClient = await connectToCluster(uri);
-    } finally {
-        await mongoClient.close();
-    }
+    await collection.insertOne(Tecnhician);
+ }
+
+ //find  all technicians from the database
+ export async function findTechnicianByName(collection, name) {
+    return collection.find({ name }).toArray();
+ }
+
+ //update a single technician by name
+ export async function updateTecnhiciansByName(collection, name, updatedFields) {
+    await collection.updateMany(
+        { name },
+        { $set: updatedFields }
+    );
+ }
+
+ //delete a single technician by name
+ export async function deleteTecnhiciansByName(collection, name) {
+    await collection.deleteMany({ name });
  }
