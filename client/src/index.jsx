45505ab4324846f4e023de13d610b99d614cc434
Sergio Rodríguez Vázquez <sergio.rodriguezvazquez@alum.uca.es>
2024-02-05 22:43:54 +0100

    Update route production

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,8 +4,8 @@ import App from './App.jsx'
 import './index.css'
 import axios  from 'axios'
 
-// axios.defaults.baseURL = 'http://13.51.242.248:8080/'
-axios.defaults.baseURL = 'http://localhost:8080/'
+axios.defaults.baseURL = 'http://13.51.242.248:8080/'
+// axios.defaults.baseURL = 'http://localhost:8080/'
 
 axios.interceptors.request.use(
   config => {
