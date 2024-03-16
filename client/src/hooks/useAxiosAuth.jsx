5c239132d6d6a35b1bf3ff1c68d91560ac04c120
SergioRgVz <sergio.rodriguezvazquez@alum.uca.es>
2024-03-16 13:31:34 +0100

    Deploy cambio ip produccion

--- client/src/hooks/useAxiosAuth.jsx
+++ client/src/hooks/useAxiosAuth.jsx
@@ -11,7 +11,7 @@ import axios from 'axios';
  */
 const apiClient = axios.create({
     // baseURL: 'http://localhost:8080/', // Desarrollo
-    baseURL: 'http://54.91.214.76:8080/', // Producción
+    baseURL: 'http://54.226.166.92:8080/', // Producción
     headers: {
         'Content-Type': 'application/json'
     }
